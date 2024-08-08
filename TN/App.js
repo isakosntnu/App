5fe8b9a4f05a57bfb0bd216984dcfafcf5a3a68a
Isak Olav Sjøberg <isakos@stud.ntnu.no>
2024-08-08 11:32:47 +0200

    working map

--- TN/App.js
+++ TN/App.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+    })();
+  }, []);
+
+  let region = {
+    latitude: 63.4305, // Trondheim coordinates
+    longitude: 10.3951,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  if (location) {
+    region = {
+      ...region,
+      latitude: location.latitude,
+      longitude: location.longitude,
+    };
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} region={region}>
+        {location && (
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+            title={"You're here"}
+          />
+        )}
+      </MapView>
     </View>
   );
 }
@@ -16,5 +58,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
